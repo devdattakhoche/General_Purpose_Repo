5b6bf51f0af1a0fde0f3e6794bb1e29d2a87a7c2
Devdatta Khoche <49261633+devdattakhoche@users.noreply.github.com>
2021-02-01 15:46:59 +0530

    Add files via upload

---
+++ wifi/wifi.ino
@@ -0,0 +1,96 @@
+#include "certs.h"
+#include <WiFiClientSecure.h>
+#include <MQTTClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
+
+
+// Wifi credentials
+const char *WIFI_SSID = "Mtnl";
+const char *WIFI_PASSWORD = "20830829";
+
+// The name of the device. This MUST match up with the name defined in the AWS console
+#define DEVICE_NAME "GOT8"
+
+// The MQTTT endpoint for the device (unique for each AWS account but shared amongst devices within the account)
+#define AWS_IOT_ENDPOINT "akhy31d6knuov-ats.iot.us-east-2.amazonaws.com"
+
+// The MQTT topic that this device should publish to
+//#define AWS_IOT_TOPIC "$aws/things/" DEVICE_NAME "/shadow/update"
+#define AWS_IOT_TOPIC "RSSI"
+
+// How many times we should attempt to connect to AWS
+#define AWS_MAX_RECONNECT_TRIES 50
+
+WiFiClientSecure net = WiFiClientSecure();
+MQTTClient client = MQTTClient(256);
+
+void connectToWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  int retries = 0;
+//  while (WiFi.status() != WL_CONNECTED && retries < 15){
+    Serial.print("Connecting to WIFI : ");
+    while (WiFi.status() != WL_CONNECTED){
+    delay(1000);
+    
+    Serial.print("*");
+    retries++;
+  }
+  if(WiFi.status() != WL_CONNECTED){
+    esp_sleep_enable_timer_wakeup(1 * 60L * 1000000L);
+    esp_deep_sleep_start();
+  }else Serial.print(" Connected!");
+}
+
+void connectToAWS()
+{
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+  client.begin(AWS_IOT_ENDPOINT, 8883, net);
+  int retries = 0;
+  Serial.print("\nConnecting to AWS IOT : ");
+
+  while (!client.connect(DEVICE_NAME) && retries < AWS_MAX_RECONNECT_TRIES) {
+    Serial.print("%");
+    delay(100);
+    retries++;
+  }
+  if(!client.connected()){
+    Serial.println(" Timeout!");
+    return;
+  }
+  Serial.println(" Connected!");
+}
+
+void sendJsonToAWS()
+{
+  StaticJsonDocument<200> jsonDoc;
+//  JsonObject stateObj = jsonDoc.createNestedObject("state");
+  
+  jsonDoc["deviceID"] = WiFi.macAddress();
+  int numberOfNetworks = WiFi.scanNetworks();
+     
+  for(int i =0; i<numberOfNetworks; i++)
+       jsonDoc[WiFi.BSSIDstr(i)] = WiFi.RSSI(i);
+
+  Serial.println("Publishing message to AWS...");
+  char jsonBuffer[512];
+  serializeJson(jsonDoc, jsonBuffer);
+
+  client.publish(AWS_IOT_TOPIC, jsonBuffer);
+}
+
+void setup() {
+  Serial.begin(9600);
+  connectToWiFi();
+  connectToAWS();
+}
+
+void loop() {
+  sendJsonToAWS();
+  client.loop();
+  delay(10000);
+}
